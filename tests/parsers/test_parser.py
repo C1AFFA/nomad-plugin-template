d2db1ed58a6c83bbe786538ef13c0b47b430f49f
Simon Ternes <ternes@ing.uniroma2.it>
2025-06-13 10:12:20 +0200

    author_id resolved

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -4,21 +4,39 @@ import sys
 import os
 from pydantic import BaseModel
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from nomad.datamodel import EntryArchive
 
 from nomad_test_parser.parsers.parser import NewParser, SimpleOutput,JVParser,RawFileUNITOV # Import SimpleOutput
 
 from nomad.datamodel import EntryArchive, EntryMetadata
 from nomad.datamodel.metainfo.workflow import Workflow
+from nomad.metainfo.metainfo import (
+    JSON,
+    Capitalized,
+    Category,
+    Datetime,
+    MCategory,
+    MSection,
+    Quantity,
+    Reference,
+    Section,
+)
+from nomad.datamodel.data import EntryData, ArchiveSection, User, UserReference, AuthorReference
+#from nomad.datamodel.datamodel import MongoUploadMetadata, EditableUserMetadata,AuthLevel;
+#from nomad.metainfo.elasticsearch_extension import Elasticsearch,DocumentType;
+
+print(EntryMetadata().main_author)
 
 
 def test_parse_file():
     parser = NewParser()
-    meta_data = EntryMetadata();
 
-    archive = EntryArchive(metadata=meta_data, workflow2=Workflow(name='test'))
-    assert not archive.metadata == None;
-    archive.metadata.user_id = 0;
+    #meta_data.main_author = UserReference()
+    #meta_data.main_author.user_id = "test"
+    archive = EntryArchive()
+    archive.metadata = EntryMetadata();
+    archive.workflow2 = Workflow(name='test')
+
+    #assert not archive.metadata.main_author == None;
 
     mainfile_path = 'tests/data/example.out' # Define mainfile_path for clarity
     parser.parse(mainfile_path, archive, logging.getLogger())
@@ -37,10 +55,19 @@ def test_parse_file():
     # then you can uncomment the line below.
     # assert archive.workflow2.name == 'test'
 
-
 def test_parse_JV_file():
+
     parser = JVParser()
-    archive = EntryArchive(metadata=EntryMetadata(), workflow2=Workflow(name='test'))
+    archive = EntryArchive()
+
+    archive.workflow2=Workflow(name='test');
+
+    meta_data = EntryMetadata();
+    meta_data.main_author = User();
+    meta_data.main_author.user_id = "42";
+
+    archive.metadata=meta_data;
+
     mainfile_path = '001_2023_10_19_18.33.25_1A_3C_C1_1_JV.txt' # Define mainfile_path for clarity
     parser.parse(mainfile_path, archive, logging.getLogger())
 
