57a3ef987a639d91594616b4ea965c2138eb1e0a
Simon Ternes <ternes@ing.uniroma2.it>
2025-07-01 17:13:01 +0200

    added MPPT

--- src/nomad_test_parser/parsers/parser.py
+++ src/nomad_test_parser/parsers/parser.py
@@ -19,7 +19,7 @@ from nomad.metainfo import Section, Quantity  # Added import
 from nomad.datamodel.data import ArchiveSection # Added import, replaces commented out #importa ArchiveSection
 from nomad.datamodel.data import EntryData
 from baseclasses.solar_energy import JVMeasurement
-from baseclasses.solar_energy import EQEMeasurement, SolarCellEQECustom
+from baseclasses.solar_energy import EQEMeasurement, SolarCellEQECustom,  MPPTrackingHsprintCustom
 from baseclasses.helper.add_solar_cell import add_band_gap
 import os;
 from baseclasses.helper.utilities import set_sample_reference, create_archive, get_entry_id_from_file_name, get_reference
@@ -31,6 +31,8 @@ from nomad.datamodel.metainfo.annotations import (
     ELNAnnotation,
 )
 from baseclasses.helper.utilities import get_encoding
+from nomad.datamodel.metainfo.plot import PlotlyFigure, PlotSection
+from nomad.units import ureg
 
 class RawFileUNITOV(EntryData):
     processed_archive = Quantity(
@@ -91,11 +93,124 @@ class UNITOV_JVmeasurement(JVMeasurement, RawFileUNITOV):
 
                 self.location = location
                 get_jv_archive(jv_dict, self.data_file, self)
-            
+
             print("=======================================WRITTEN DATA IN ARCHIVE")
 
         super(UNITOV_JVmeasurement,
               self).normalize(archive, logger)
+
+
+class UNITOV_MPPTracking_Measurement(MPPTrackingHsprintCustom, PlotSection, RawFileUNITOV):
+    m_def = Section(
+        a_eln=dict(
+            hide=[
+                'lab_id',
+                'solution',
+                'users',
+                'author',
+                'end_time',
+                'steps',
+                'instruments',
+                'results',
+                'location',
+                'figures',
+            ],
+            properties=dict(
+                order=[
+                    'name',
+                    'data_file',
+                    'active_area',
+                    'intensity',
+                    'integration_time',
+                    'settling_time',
+                    'averaging',
+                    'compliance',
+                    'samples',
+                ]
+            ),
+        )
+    )
+
+    def normalize(self, archive, logger):
+        from baseclasses.helper.archive_builder.mpp_hysprint_archive import get_mpp_hysprint_samples
+        from baseclasses.helper.utilities import rewrite_json
+
+        from nomad_test_parser.parsers.file_reading import read_mppt_file
+
+        if self.data_file and self.load_data_from_file:
+            self.load_data_from_file = False
+
+            #rewrite_json(['data', 'load_data_from_file'], archive, False)
+
+            # from baseclasses.helper.utilities import get_encoding
+            # with archive.m_context.raw_file(self.data_file, "br") as f:
+            #     encoding = get_encoding(f)
+
+            with archive.m_context.raw_file(self.data_file, 'tr', encoding='ascii') as f:
+                if os.path.splitext(f.name)[-1] != '.csv':
+                    return
+
+                data = read_mppt_file(f.read())  # , encoding)
+
+            self.samples = get_mpp_hysprint_samples(self, data)
+
+        import pandas as pd
+        import plotly.express as px
+
+        column_names = ['Time [hr]', 'Efficiency [%]', 'P']
+        self.figures = []
+        if self.averages:
+            fig = px.scatter()
+            df = pd.DataFrame(columns=column_names)
+            for avg in self.averages:
+                df1 = pd.DataFrame(columns=column_names)
+                df1[column_names[0]] = avg.time * ureg('hr')
+                df1[column_names[1]] = avg.efficiency
+                df1[column_names[2]] = avg.name
+                df = pd.concat([df, df1])
+
+            fig = px.scatter(
+                df,
+                x=column_names[0],
+                y=column_names[1],
+                color=column_names[2],
+                symbol=column_names[2],
+                title='Averages',
+            )
+            fig.update_traces(marker=dict(size=4))
+            fig.update_layout(
+                showlegend=True,
+                xaxis=dict(fixedrange=False),
+                yaxis=dict(fixedrange=False),
+            )
+            self.figures.append(PlotlyFigure(label='Averages', index=0, figure=fig.to_plotly_json()))
+
+        if self.best_pixels:
+            df = pd.DataFrame(columns=column_names)
+            for bp in self.best_pixels:
+                df1 = pd.DataFrame(columns=column_names)
+                df1[column_names[0]] = bp.time * ureg('hr')
+                df1[column_names[1]] = bp.efficiency
+                df1[column_names[2]] = bp.name
+                df = pd.concat([df, df1])
+
+            fig = px.scatter(
+                df,
+                x=column_names[0],
+                y=column_names[1],
+                color=column_names[2],
+                symbol=column_names[2],
+                title='Best Pixels',
+            )
+            fig.update_traces(marker=dict(size=4))
+            fig.update_layout(
+                showlegend=True,
+                xaxis=dict(fixedrange=False),
+                yaxis=dict(fixedrange=False),
+            )
+            self.figures.append(PlotlyFigure(label='Best Pixel', index=1, figure=fig.to_plotly_json()))
+
+        super().normalize(archive, logger)
 
 class UNITOV_EQEmeasurement(EQEMeasurement, RawFileUNITOV):
     m_def = Section(
@@ -233,41 +348,34 @@ class EQEParser(MatchingParser):
         #entry = UNITOV_JVmeasurement()
         notes = ''
 
-        # 2. Populate the section with some data
-        #    (In a real parser, this data would come from parsing the 'mainfile')
 
-        #archive.metadata.entry_name = os.path.basename(mainfile)
-
-        #mainfile_split = mainfile.split('.');
-
-        #if not mainfile_split[-1] in ["nk"]:
-        #    search_id = mainfile_split[0]
-        #    set_sample_reference(archive, entry, search_id)
-
-        #    entry.name = f"{search_id} {notes}"
-        #    entry.description = f"Notes from file name: {notes}"
-
-        #if not measurment_type in ["uvvis", "sem", "SEM"]:
-        #    entry.data_file = os.path.basename(mainfile)
-
-
-
-        #file_name = f'{os.path.basename(mainfile)}.archive.json'
-        #eid = get_entry_id_from_file_name(file_name, archive)
-
-        #archive.data = UNITOV_JVmeasurement(processed_archive=get_reference(archive.metadata.upload_id, eid))
         archive.data = UNITOV_EQEmeasurement()
         archive.data.message = 'This is a test EQE measurement parsing.'
         archive.data.datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         archive.data.data_file = mainfile;
         archive.data.m_to_dict(with_root_def=True)
 
-        #create_archive(entry, archive, file_name)
+class MPPTParser(MatchingParser):
+    def parse(
+        self,
+        mainfile: str,
+        archive: 'EntryArchive',
+        logger: 'BoundLogger',
+        child_archives: dict[str, 'EntryArchive'] = None,
+    ) -> None:
+        logger.info('MPPTParser.parse', parameter=configuration.parameter)
+        print("Parse MPPT ==========================================")
 
-        # 3. Assign your custom section to archive.data
-        #    archive.data is the typical place for the primary data extracted by the parser.
-        #archive.data = my_simple_output_data
+        # 1. Create an instance of your custom section
+        #entry = UNITOV_JVmeasurement()
+        notes = ''
 
+
+        archive.data = UNITOV_MPPTracking_Measurement()
+        archive.data.message = 'This is a test MPPT measurement parsing.'
+        archive.data.datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        archive.data.data_file = mainfile;
+        archive.data.m_to_dict(with_root_def=True)
 
 
 class JVParser(MatchingParser):
--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -5,7 +5,7 @@ import os
 from pydantic import BaseModel
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from nomad_test_parser.parsers.parser import NewParser, SimpleOutput,JVParser,EQEParser,RawFileUNITOV,UNITOV_JVmeasurement # Import SimpleOutput
+from nomad_test_parser.parsers.parser import NewParser, SimpleOutput,JVParser,EQEParser,MPPTParser,RawFileUNITOV,UNITOV_JVmeasurement # Import SimpleOutput
 
 from nomad.datamodel import EntryArchive, EntryMetadata
 from nomad.datamodel.metainfo.workflow import Workflow
@@ -144,14 +144,39 @@ def test_parse_EQE_file():
     # Check if archive.data is an instance of RawFileUNITOV
     assert isinstance(archive.data, RawFileUNITOV)
 
-    # Check the populated values in archive.data
+def test_parse_MPPT_file():
+    try:
+        nested_directory_path = Path(".volumes/fs/staging/42/42")
+        # Create nested directories
+        nested_directory_path.mkdir(parents=True, exist_ok=True)
+        #os.mkdirs('.volumes/fs/staging/42/42')
+        print(f"Directory created successfully.")
+    except FileExistsError:
+        print(f"Directory .volumes/fs/staging/42/42 already exists.")
 
-    #assert archive.data.message == 'This is a test message from NewParser.'
-    #assert archive.data.example_value == 123.456
-    #assert archive.data.parsed_mainfile_path == mainfile_path
+    mainfile_path = '000_2023_10_19_18.33.10_1A_3C_C1_1_Tracking.txt'
+    parser = MPPTParser()
+    context = ServerContext(Upload(upload_id="42"))
+    context.uploadFiles = UploadFiles.get("42",create=True)
 
-    # The assertion for workflow2.name can remain if it's still relevant
-    # For the current parser.py, workflow2 is not being set, so this would fail.
-    # If you re-add `archive.workflow2 = Workflow(name='test')` to your parser,
-    # then you can uncomment the line below.
-    # assert archive.workflow2.name == 'test'
+    archive = EntryArchive(m_context=context)
+    archive.metadata = EntryMetadata();
+    archive.metadata.entry_name = 'test'
+    archive.workflow2=Workflow(name='test');
+
+    #meta_data = EntryMetadata();
+    #meta_data.main_author = User();
+    #meta_data.main_author.user_id = "42";
+
+    #archive.metadata=meta_data;
+
+     # Define mainfile_path for clarity
+    parser.parse(mainfile_path, archive, logging.getLogger())
+
+    #Check if datafile available
+    assert archive.data.data_file;
+
+    archive.data.normalize(archive, logging.getLogger());
+
+    # Check if archive.data is an instance of RawFileUNITOV
+    assert isinstance(archive.data, RawFileUNITOV)
